4ab2fe3e231d0db453b82052d6ee214615dd771a
Vadim Demedes <vdemedes@gmail.com>
2017-07-15 21:39:08 +0300

    fix

--- src/progress-bar.js
+++ src/progress-bar.js
@@ -10,7 +10,8 @@ class Bar extends Component {
   getString() {
     const {percent, columns, left, right, character} = this.props;
 
-    const space = columns - right - left;
+    const screen = columns || process.stdout.columns || 80;
+    const space = screen - right - left;
     const max = Math.min(Math.floor(space * percent), space);
 
     return character.repeat(max);
@@ -23,7 +24,7 @@ class Bar extends Component {
 }
 
 Bar.defaultProps = {
-  columns: process.stdout.columns || 80,
+  columns: 0,
   percent: 1,
   left: 0,
   right: 0,
