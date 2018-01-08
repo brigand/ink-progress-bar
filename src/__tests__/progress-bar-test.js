38771fd2f3d2e5d40535fb979ca302f5495f5068
yuffiy <yfhj1990@hotmail.com>
2018-01-08 16:40:13 +0800

    Add right pad prop.

--- src/__tests__/progress-bar-test.js
+++ src/__tests__/progress-bar-test.js
@@ -1,12 +1,13 @@
 const ProgressBar = require('../progress-bar.js');
 
-const run = (columns, left, right) => ProgressBar.prototype.getString.call({
+const run = (columns, left, right, percent = 1, rightPad = false) => ProgressBar.prototype.getString.call({
   props: {
     columns,
     left,
     right,
-    percent: 1,
-    character: 'x'
+    percent,
+    character: 'x',
+    rightPad
   }
 });
 
@@ -15,5 +16,13 @@ it(`has correct length`, () => {
   expect(str.length).toBe(50);
 
   const str2 = run(60, 10, 9);
+  expect(str2.length).toBe(41);
+});
+
+it(`should pad right space`, () => {
+  const str = run(50, 0, 0, 0.88, true);
+  expect(str.length).toBe(50);
+
+  const str2 = run(60, 10, 9, 0.42, true);
   expect(str2.length).toBe(41);
 });
